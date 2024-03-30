3817848ae0e461d637193ebede20d5051d670e11
joe <>
2024-03-30 22:45:42 +0800

    微調 skeleton 效果

--- src/app/options/skeleton/skeleton.component.ts
+++ src/app/options/skeleton/skeleton.component.ts
@@ -12,11 +12,11 @@ import { TableModule } from 'primeng/table';
     TableModule
   ],
   template: `
-    <div class="border-round border-1 surface-border p-4 surface-card" style="width:900px;height:515px;">
+    <div class="border-round border-1 surface-border p-4 surface-card" style="width:900px;height:640px;">
         <div class="flex justify-content-between align-items-center mb-3">
             <div class="flex align-items-center"> 
               <p-skeleton size="2.5rem" styleClass="mr-2"></p-skeleton>
-              <p-skeleton width="10rem" height="3.5rem"></p-skeleton>
+              <p-skeleton width="14rem" height="3rem"></p-skeleton>
             </div>
             <div>
               <p-skeleton size="3rem" styleClass="mr-2"></p-skeleton>
@@ -35,28 +35,28 @@ import { TableModule } from 'primeng/table';
           </ng-template>
           <ng-template pTemplate="body" let-product>
               <tr>
-                  <td><p-skeleton  width="2rem"></p-skeleton></td>
+                  <td><p-skeleton size="2rem" styleClass="mr-2"></p-skeleton></td>
                   <td><p-skeleton  width="2rem"></p-skeleton></td>
                   <td><p-skeleton  width="9rem"></p-skeleton></td>
-                  <td><p-skeleton width="30rem"></p-skeleton></td>
-                  <td><p-skeleton  width="5rem"></p-skeleton></td>
-                  <td><p-skeleton></p-skeleton></td>
+                  <td><p-skeleton width="29rem"></p-skeleton></td>
+                  <td class="flex">
+                    <p-skeleton size="2rem" styleClass="mr-2"></p-skeleton>
+                    <p-skeleton size="2rem" styleClass="mr-2"></p-skeleton>
+                  </td>
+                  <td><p-skeleton width="2rem"></p-skeleton></td>
               </tr>
           </ng-template>
       </p-table>
         <div class="flex justify-content-between align-items-center mt-3">
-            <p-skeleton width="8rem" height="2.8rem"></p-skeleton>
-            <p-skeleton width="9rem" height="2rem"></p-skeleton>
+            <p-skeleton width="8rem" height="2.3rem"></p-skeleton>
+            <p-skeleton width="9rem" height="1.5rem"></p-skeleton>
         </div>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SkeletonComponent {
-  fakeDate = Array.from({ length: 7 }).map((_, i) => ({
-    code: i + 1,
-    name: 'Product Name',
-    category: 'Category',
-    quantity: 10
+  fakeDate = Array.from({ length: 8 }).map((_, i) => ({
+    code: i + 1
   }));
 }
