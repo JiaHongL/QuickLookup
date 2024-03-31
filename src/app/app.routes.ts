1a635f36db4790067ea072dd5ec96e6e7644ce1e
joe <>
2024-03-31 17:07:27 +0800

    刪除 home & options_page setting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { PopupComponent } from './pages/popup/popup.component';
-import { HomeComponent } from './pages/home/home.component';
 import { OptionsComponent } from './pages/options/options.component';
 
 export const routes: Routes = [
     { path: 'popup', component: PopupComponent },
     { path: 'options' , component: OptionsComponent },
-    { path: 'home' , component: HomeComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'options', pathMatch: 'full' },
 ];
